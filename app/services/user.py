aec5b3c7fdc5ac10c4378c8824831a0058ebd3a6
weijay <weijay0804@gmail.com>
2024-06-15 00:25:24 +0800

    feat: 新增 註冊用戶帳號 API

--- app/services/user.py
+++ app/services/user.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
+from app import security
 from app.crud import user_crud
 from app.models.user import User
 from app.schemas import db_schemas, request_schemas
@@ -13,8 +14,8 @@ def create_user_account(data: request_schemas.UserCreateAccountResp, session: Se
     if user_exist:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email is already exists.")
 
-    # TODO password 要 hash
-    obj_in = db_schemas.UserDBCreate(email=data.email, name=data.name, password=data.password)
+    hashed_pwd = security.hash_password(data.password)
+    obj_in = db_schemas.UserDBCreate(email=data.email, name=data.name, password=hashed_pwd)
 
     user = user_crud.create(session, obj_in=obj_in)
 
